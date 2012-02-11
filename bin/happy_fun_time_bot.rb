264f73d9f85b2f98e9ba4b991039da0f73954c64
Brent Wooden <brent.wooden@gmail.com>
2012-02-11 09:27:54 -0800

    Adding config yml and bin directory and file.

---
+++ bin/happy_fun_time_bot.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/env ruby
+require 'rubygems'
+require '../lib/happy_fun_time_bot.rb'
+
+# For hipchat, your :jid, :room and :password are available at https://www.hipchat.com/account/xmpp
+config = YAML.load_file('../config/hipchat.yml')
+
+@bot = HappyFunTimeBot.new(
+  :jid => config["jid"], 
+  :nick => config["nick"], 
+  :room => config["room"], 
+  :password => config["password"]
+)
+
+# let's find some images!
+@bot.add_responder('image') do |from, args|
+  url = Google.get_image_url(args)
+  "Look! #{url}"
+end
+
+@bot.run!
